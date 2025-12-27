18651b371433e4e5cc28ca42e44038be3eaed101
Irene Chen <85856587+i-r-e-n-e@users.noreply.github.com>
2025-12-27 22:49:32 +0900

    adding images to home page

--- elainePortfolio/src/pages/Home.jsx
+++ elainePortfolio/src/pages/Home.jsx
@@ -1,9 +1,45 @@
 import Hero from "../components/Hero"
+import rue from "../assets/restingPlace/rue.png"
+import shuyin from "../assets/restingPlace/shuyin.png"
+import cats from "../assets/restingPlace/cats.png"
+import gamePlay from "../assets/restingPlace/gameplay.png"
+import gamePlayUI from "../assets/restingPlace/gameplayui.png"
+import facades from "../assets/restingPlace/facades.png"
+import blueSkySketches from "../assets/restingPlace/blueskysketches.png"
+
 
 export default function Home() {
   return (
     <main>
-      <Hero/>
+        <Hero/>
+        <img
+            src={rue}
+            alt="Rue"
+        />
+        <img
+            src={shuyin}
+            alt="Shuyin"
+        />
+        <img
+            src={cats}
+            alt="Cats"
+        />
+        <img
+            src={gamePlay}
+            alt="Game Play"
+        />
+        <img
+            src={gamePlayUI}
+            alt="Game Play UI"
+        />
+        <img
+            src={facades}
+            alt="Facades"
+        />
+        <img
+            src={blueSkySketches}
+            alt="Blue Sky Sketches"
+        />
     </main>
   );
 }
